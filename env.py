15d03b6780f66d131a8c2f8a7c1074af54e702d9
Vinicius Pontarolla <103379493+Vepp1@users.noreply.github.com>
2023-01-21 17:45:59 +0100

    Update issue templates

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['CLOUDINARY_URL'] = 'cloudinary://852691531612292:DShrlel0MfOhu9YICRuprm0WOFc@dzd4izfmm'
+os.environ['SECRET_KEY'] = 'g56fq7y0(ee&!@3gc3x#moa&01i@0i%6=_89aaf*f4hi*w*vi&s'
