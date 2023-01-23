c9d004ab9759fbd5318789d5028ca16880abc970
Vepp <vinipontarolla@gmail.com>
2023-01-23 16:58:34 +0100

    Add IsOwnerOrReadOnly permission to prevent a user to modify other users profile

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Profile
 from .serializers import ProfileSerializer
+from cinemary_api.permissions import IsOwnerOrReadOnly
 
 
 
@@ -13,3 +14,5 @@ class ProfileList(generics.ListAPIView):
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
+
