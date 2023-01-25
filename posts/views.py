e056efb2c24d76fab97a47786846edf50e798194
Vepp <vinipontarolla@gmail.com>
2023-01-25 17:56:52 +0100

    Add posts views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from .models import Posts
+from .serializers import PostSerializer
+from cinemary_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Post.objects.all().order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
