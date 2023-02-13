f836da188ced29f312ed5a83a7e5184979f0f334
Vepp <vinipontarolla@gmail.com>
2023-02-13 17:47:27 +0100

    Add LikeList view

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Likes
+from .serializers import LikesSerializer
+from cinemary_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self.serializer):
+        serializer.save(owner=self.request.user)
