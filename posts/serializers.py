c70e6354bd55b22de5de507e99a0d90c2014615b
Vepp <vinipontarolla@gmail.com>
2023-02-13 20:22:24 +0100

    Add function to get a like id from each post

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,16 +1,25 @@
 
 from rest_framework import serializers
 from posts.models import Posts
+from likes.models import Likes
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    like_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Likes.objects.filter(owner=user, post=obj).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Posts
@@ -18,4 +27,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'created_at', 'updated_at',
             'title', 'content', 'image', 'genrer',
+            'like_id',
         ]
