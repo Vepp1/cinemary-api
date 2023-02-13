1bd15b5e0a881622ccdb9de219a563223135cb14
Vepp <vinipontarolla@gmail.com>
2023-02-13 17:54:55 +0100

    Add LikeDetail view

--- likes/views.py
+++ likes/views.py
@@ -11,3 +11,9 @@ class LikeList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
