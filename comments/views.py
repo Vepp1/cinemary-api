6031bdf18f8b70ec458a216827ad21b6b62c395a
Vepp <vinipontarolla@gmail.com>
2023-02-16 14:49:47 +0100

    Add comments list and detail views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Comments
+from .serializers import CommentsSerializer
+from cinemary_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentsSerializer
+    queryset = Comments.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = CommentsSerializer
+    queryset = Comments.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
