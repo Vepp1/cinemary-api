9a45a2b7c6f9dbc27ec7cf102b83bbce7c0fd88b
Vepp <vinipontarolla@gmail.com>
2023-02-22 14:49:45 +0100

    Add django filters to post to filter posts by owner username, title, or genrer

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Posts
 from .serializers import PostSerializer
 from cinemary_api.permissions import IsOwnerOrReadOnly
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostList(generics.ListCreateAPIView):
@@ -12,6 +13,15 @@ class PostList(generics.ListCreateAPIView):
     ]
     queryset = Posts.objects.annotate(likes_count=Count(
         'likes', distinct=True), comments_count=Count('comments', distinct=True),).order_by('-created_at')
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+        'genrer',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
