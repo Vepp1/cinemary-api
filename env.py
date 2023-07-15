27d0e4598386dacce5ed7441984163c09987afee
Vepp1 <vinipontarolla@gmail.com>
2023-07-15 13:58:28 +0000

    Remove cached env

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['CLOUDINARY_URL'] = 'cloudinary://852691531612292:DShrlel0MfOhu9YICRuprm0WOFc@dzd4izfmm'
-os.environ['DATABASE_URL'] = 'postgres://ubofumep:byKNcca4jQc5lf8Ww6j3WUNlj0bauexk@snuffleupagus.db.elephantsql.com/ubofumep'
-os.environ['SECRET_KEY'] = 'g0b7r7y0(ee&!@3gc3x#moa&01i@0i%6=_89aaf*f4hi*w*vi&s'
